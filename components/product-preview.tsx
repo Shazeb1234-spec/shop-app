5d2e21e68dcccc3c8d77fa1a5a2a3179ba1c9379
Shazeb <Shazeb.arafin@gmail.com>
2025-12-27 18:21:54 +0400

    Shop

--- components/product-preview.tsx
+++ components/product-preview.tsx
@@ -1,10 +1,19 @@
 import Image from 'next/image'
 import React from 'react'
+import { ToggleGroup, ToggleGroupItem } from './ui/toggle-group'
+import { BookmarkIcon, HeartIcon, Star, StarIcon } from 'lucide-react'
 
 const ProductPreview = () => {
   return (
     <div>
-        <Image src={"/placeholder.jpg"} alt="placeholder" width={500} height={1000} />
+      <Image src={"/placeholder.jpg"} alt="placeholder" width={500} height={1000} />
+      <h4 className='font-semibold text-2xl'>Red normal T-shirt</h4>
+      <div className='grid grid-flow-col justify-start gap-2'>
+        {[1, 2, 3, 4, 5].map((i) => (
+          <Star key={i} className='size-4 fill-amber-400 text-amber-400' />
+        ))}
+      </div>
+      <h4 className='font-serif font-semibold text-xl'>$25.85</h4>
     </div>
   )
 }
