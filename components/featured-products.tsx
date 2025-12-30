6d0e47741a0ad12e63cb86f8561c247aac526b0a
Shazeb <Shazeb.arafin@gmail.com>
2025-12-30 20:20:22 +0400

    Completed featured products

--- components/featured-products.tsx
+++ components/featured-products.tsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import { Separator } from './ui/separator'
-import ProductPreview from './product-preview'
+import ProductPreview, { ProductPreviewProps } from './product-preview'
 
 const FeaturedProducts = () => {
+    const products: ProductPreviewProps[] = [
+        {
+            src: "/product-12.jpg",
+            title: "Sweat Pants",
+            stars: 4,
+            price: 25.66
+        },
+        {
+            src: "/placeholder.jpg",
+            title: "Cool Red Shoes",
+            stars: 5,
+            price: 65.99
+        },
+        {
+            src: "/product-4.jpg",
+            title: "blue normal t-shirt",
+            stars: 3,
+            price: 30.25
+        },
+    ]
     return (
         <div className='grid gap-3 align-middle justify-center '>
             <div className='w-40 justify-self-center'>
                 <Separator className="my-4 border-7 rounded-lg" />
             </div>
             <h2 className='text-3xl font-extrabold text-center'>Featured Products</h2>
-            <ProductPreview/>
+            <div className='grid lg:grid-cols-3 gap-3'>
+                {Array.from({ length: products.length }, (_, i) => (
+                    <ProductPreview key={i} Value={products[i]} />
+                ))}
+            </div>
         </div>
     )
 }
--- components/product-preview.tsx
+++ components/product-preview.tsx
@@ -3,17 +3,27 @@ import React from 'react'
 import { ToggleGroup, ToggleGroupItem } from './ui/toggle-group'
 import { BookmarkIcon, HeartIcon, Star, StarIcon } from 'lucide-react'
 
-const ProductPreview = () => {
+export interface ProductPreviewProps{
+  src: string,
+  title: string,
+  stars: number,
+  price: number, 
+
+}
+
+const ProductPreview = ({Value}:{Value: ProductPreviewProps}) => {
+  
   return (
     <div>
-      <Image src={"/placeholder.jpg"} alt="placeholder" width={500} height={1000} />
-      <h4 className='font-semibold text-2xl'>Red normal T-shirt</h4>
+
+      <Image src={Value.src||"/placeholder.jpg"} alt="placeholder" width={500} height={1000} />
+      <h4 className='font-semibold text-2xl'>{Value.title}</h4>
       <div className='grid grid-flow-col justify-start gap-2'>
-        {[1, 2, 3, 4, 5].map((i) => (
+        {Array.from({length:Value.stars},(_,i) => (
           <Star key={i} className='size-4 fill-amber-400 text-amber-400' />
         ))}
       </div>
-      <h4 className='font-serif font-semibold text-xl'>$25.85</h4>
+      <h4 className='font-serif font-semibold text-xl'>${Value.price}</h4>
     </div>
   )
 }
